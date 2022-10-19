2e96b82cf70dedee1237546511fa87d4cd89657c
Pedro Plácido <up202107987@up.pt>
2022-10-19 12:54:10 +0100

    Third

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -1,2 +1,50 @@
-package PACKAGE_NAME;public class Game {
+import com.googlecode.lanterna.TerminalSize;
+import com.googlecode.lanterna.TextCharacter;
+import com.googlecode.lanterna.screen.Screen;
+import com.googlecode.lanterna.screen.TerminalScreen;
+import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
+import com.googlecode.lanterna.terminal.Terminal;
+
+import javax.swing.*;
+import java.io.IOException;
+
+public class Game {
+    private Screen screen;
+
+    private static int x = 10;
+    private static int y = 10;
+
+
+    public static void main(String[] args) {
+        try {
+            TerminalSize terminalSize = new TerminalSize(40, 20);
+            DefaultTerminalFactory terminalFactory = new DefaultTerminalFactory().setInitialTerminalSize(terminalSize);
+            Terminal terminal = terminalFactory.createTerminal();
+            Screen screen = new TerminalScreen(terminal);
+            screen.setCursorPosition(null); // we don't need a cursor
+            screen.startScreen(); // screens must be started
+            screen.doResizeIfNecessary(); // resize screen if necessary
+            screen.setCharacter(x, y, TextCharacter.fromCharacter('X')[0]);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void draw() throws IOException{
+        screen.clear();
+        screen.setCharacter(10, 10, TextCharacter.fromCharacter('X')[0]);
+        screen.refresh();
+    }
+
+    public void run() throws IOException{
+        while(true){
+            draw();
+        }
+    }
+
+    private void processKey(KeyStroke key) {
+        System.out.println(key);
+    }
+
+
 }
